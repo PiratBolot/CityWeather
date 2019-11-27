42957700e73ff51d7cc3250074305cc6bcd703f9
PiratBolot <remedictes@gmail.com>
2019-11-27 12:32:55 +0300

    Повешен обработчик на submit у формы Обработчик получает cityName из event Разбиты функции в index.js

--- src/index.js
+++ src/index.js
@@ -2,36 +2,40 @@ import './scss/base.scss'
 
 function getCityData(city) {
     return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=5c421a898af8f8f0d9a04eb07a32545d&units=metric`)
-        .then(data => data.json())
         .then(data => {
-            const first = data.list[0];
-            return {
-                city: data.city.name,
-                date: Date(first.dt),
-                temperature: `${first.main.temp} C`,
-                humidity: `${first.main.humidity} %`,
-                pressure: `${first.main.pressure} hPa`,
-                wind: `${first.wind.speed} meter/sec`,
-            };
+            return data.json()
         })
         .catch(reason => {
             return null;
         })
 }
 
-function update() {
-    getCityData(document.getElementById('form-input').value)
+function formatData(data) {
+    if (data.cod !== "200") {
+        return null;
+    }
+    const first = data.list[0];
+    return {
+        city: data.city.name,
+        date: Date(first.dt),
+        temperature: `${first.main.temp} C`,
+        humidity: `${first.main.humidity} %`,
+        pressure: `${first.main.pressure} hPa`,
+        wind: `${first.wind.speed} meter/sec`,
+    };
+}
+
+function update(event) {
+    getCityData(event.target['form-input'].value)
         .then(context => {
             let source;
-            if (context == null)
+            if (context.cod !== "200")
                 source = document.getElementById('entry-template-on-error').innerHTML;
             else
                 source = document.getElementById('entry-template').innerHTML;
             const template = Handlebars.compile(source);
-            document.getElementById('result').innerHTML = template(context);
+            document.getElementById('result').innerHTML = template(formatData(context));
         })
 }
 
-document.getElementById("search").onclick = function () {
-    update();
-}
+document.getElementById("search-form").addEventListener('submit', event => update(event));
